b1e6d1a01429d1f659b4111da14eed609cbf6d1b
iokilo <jake672005@gmail.com>
2026-02-21 13:48:02 -0500

    Finished schema

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -6,3 +6,16 @@ CREATE TABLE users (
   kdf_params TEXT NOT NULL,
   rfid_val TEXT
 )
+
+CREATE TABLE vault_entries (
+  entry_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_id INTEGER NOT NULL,
+  name TEXT NOT NULL,
+  username TEXT NOT NULL,
+  password TEXT NOT NULL,
+  url TEXT NOT NULL,
+  notes TEXT,
+  last_updated TEXT NOT NULL DEFAULT (datetime('now', 'localtime')),
+  created_at TEXT NOT NULL DEFAULT (datetime('now', 'localtime')),
+  FOREIGN KEY (user_id) REFERENCES users (user_id),
+)
